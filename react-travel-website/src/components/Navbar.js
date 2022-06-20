6b2464df40f81067e89d39df6f9b17859e03aac9
email <abe.solis2015@gmail.com>
2022-06-20 15:07:38 -0700

    Nav bar functions

---
+++ react-travel-website/src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+  //Use State for hamburger menu
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to='/' className="navbar-logo">
+            DSTN <i className="fab fa-typo3"></i>
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nave-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
