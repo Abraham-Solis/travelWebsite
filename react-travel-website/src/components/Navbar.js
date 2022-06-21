d38a5bd2f7665daa55fe8966dad4faf55f9806d3
email <abe.solis2015@gmail.com>
2022-06-20 17:12:48 -0700

    Fixed Navbar Links

--- react-travel-website/src/components/Navbar.js
+++ react-travel-website/src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Voyage
-            <i class="fa-solid fa-anchor"/>
+            <i className="fa-solid fa-anchor"/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
